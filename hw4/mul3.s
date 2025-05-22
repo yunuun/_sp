19717f1a52f3b34b1e53956d1bbbcf82938ac5ca
yunuun <avril931121@gmail.com>
2025-05-22 20:22:20 +0800

    hw4

---
+++ hw4/mul3.s
@@ -0,0 +1,8 @@
+        .global mul3
+
+        .text
+mul3:
+        mov %rdi, %rax      /* %rdi -> %rax */
+        imulq %rsi, %rax    /* %rax = %rax * %rsi */
+        imulq %rdx, %rax    /* %rax = %rax * %rdx */
+        ret
